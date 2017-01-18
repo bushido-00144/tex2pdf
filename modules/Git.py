2604dc19f4701190086b73e5fd90834ccf516e24
bushido <bushido.00144@gmail.com>
2017-01-18 22:29:39 +0900

    add username to repo_path

--- modules/Git.py
+++ modules/Git.py
@@ -3,11 +3,12 @@ import os
 def GitClone(repository_url, repo_path):
     os.system('git clone ' + repository_url + ' ' + repo_path)
 
-def GitPull(repository_url):
+def GitPull(repository_url, username):
     repository_name = repository_url.split('/')[-1].replace('.git', '')
     script_dir = os.path.abspath(os.path.dirname(__file__))
-    repositoory_dir = '/'.join(script_dir.split('/')[:-1]) + '/repos/' + repository_name
+    repositoory_dir = '/'.join(script_dir.split('/')[:-1]) + '/repos/' + username + '/' + repository_name
     if os.path.exists(repositoory_dir):
+        os.chdir(repositoory_dir)
         os.system('git pull origin master')
     else:
         GitClone(repository_url, repositoory_dir)
