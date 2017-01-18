27df6509d1d3d0be75d28868023a55209ab3251d
bushido <bushido.00144@gmail.com>
2017-01-18 23:21:05 +0900

    Tex2pdf to be return pdf file path

--- modules/Tex2pdf.py
+++ modules/Tex2pdf.py
@@ -5,5 +5,9 @@ def tex2pdf(repository_dir):
         os.chdir(repository_dir)
         os.system('make')
     except:
-        return False
-    return True
+        return 'ERROR'
+    file_list = os.listdir(repository_dir)
+    for f in file_list:
+        if 'abst.pdf' in f:
+            return repository_dir + '/' + 'abst.pdf'
+    return 'ERROR'
