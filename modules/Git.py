c1e11bcd14f1de6c902557c0ff05bd016272bdfe
bushido <bushido@klab.ai.kyutech.ac.jp>
2017-01-22 14:51:09 +0900

    change key storage location users/<username>/keys/ -> ~/.ssh/keys/<username>

--- modules/Git.py
+++ modules/Git.py
@@ -1,15 +1,36 @@
 import os
 
-def GitClone(repository_url, repo_path):
-    os.system('git clone ' + repository_url + ' ' + repo_path)
+
+def GitClone(repository_url, repo_path, git_ssh_script):
+    clone_command = 'GIT_SSH=%s git clone ' % git_ssh_script
+    os.system(clone_command + repository_url + ' ' + repo_path)
+
 
 def GitPull(repository_url, username):
     repository_name = repository_url.split('/')[-1].replace('.git', '')
     script_dir = os.path.abspath(os.path.dirname(__file__))
-    repositoory_dir = '/'.join(script_dir.split('/')[:-1]) + '/users/' + username + '/repos/' + repository_name
+    user_dir = '/'.join(script_dir.split('/')[:-1]) + '/users/' + username
+    repositoory_dir = user_dir + '/repos/' + repository_name
+    git_ssh_script = user_dir + '/git-ssh.sh'
     if os.path.exists(repositoory_dir):
         os.chdir(repositoory_dir)
-        os.system('git pull origin master')
+        os.system('GIT_SSH=%s git pull origin master' % git_ssh_script)
     else:
-        GitClone(repository_url, repositoory_dir)
+        GitClone(repository_url, repositoory_dir, git_ssh_script)
     return repositoory_dir
+
+
+def createGitSSH(user_dir):
+    home_dir = os.path.expanduser('~')
+    username = user_dir.split('/')[-1]
+    key_path = home_dir + '/.ssh/keys' + username + 'id_rsa'
+    script_path = user_dir + '/git-ssh.sh'
+    script = '''
+    #!/bin/sh
+    exec ssh -o IdentityFile=%s "$@"
+    ''' % key_path
+    fd = open(script_path, 'a')
+    fd.write(script)
+    fd.close()
+    os.system('chmod 755 %s' % script_path)
+    return script_path
--- modules/Key.py
+++ modules/Key.py
@@ -1,24 +1,27 @@
 import os
 
+
 def createKey(username):
-    script_dir = os.path.abspath(os.path.dirname(__file__))
-    key_dir = '/'.join(script_dir.split('/')[:-1]) + '/users/' + username + '/keys/'
+    home_dir = os.path.expanduser('~')
+    key_dir = home_dir + '/.ssh/keys/' + username
     os.mkdir(key_dir)
-    key_filename = key_dir + 'id_rsa'
+    key_filename = key_dir + '/id_rsa'
     os.system('ssh-keygen -q -f ' + key_filename + ' -N \'\'')
     fd = open(key_filename + '.pub')
     pub_key = fd.read()
     fd.close()
     return pub_key
 
+
 def getPubKey(username):
-    script_dir = os.path.abspath(os.path.dirname(__file__))
-    key_dir = '/'.join(script_dir.split('/')[:-1]) + '/users/' + username + '/keys/'
-    key_filename = key_dir + 'id_rsa'
+    home_dir = os.path.expanduser('~')
+    key_dir = home_dir + '/.ssh/keys/' + username
+    key_filename = key_dir + '/id_rsa'
     try:
         fd = open(key_filename + '.pub')
         pub_key = fd.read()
         fd.close()
+        print(pub_key)
         return pub_key
     except:
         return 'ERROR'
