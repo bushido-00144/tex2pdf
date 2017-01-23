9c83625b756d25d4a6de672ae1e3234e0daf512f
bushido <bushido@klab.ai.kyutech.ac.jp>
2017-01-23 14:10:56 +0900

    Ignore command Error(tmp)

--- modules/Tex2pdf.py
+++ modules/Tex2pdf.py
@@ -2,14 +2,6 @@ import os
 import shutil
 
 def tex2pdf(repository_dir):
-    try:
-        os.chdir(repository_dir)
-        os.system('make')
-        shutil.move(repository_dir+'/abst.pdf', '../../files/abst.pdf')
-    except:
-        return 'ERROR'
-    file_list = os.listdir(repository_dir)
-    if 'abst.pdf' in file_list:
-        pdf_file = '/'.join(os.getcwd().split('/')[:-2]) + '/abst.pdf'
-        return pdf_file
-    return 'ERROR'
+    os.chdir(repository_dir)
+    os.system('make')
+    shutil.move(repository_dir+'/abst.pdf', '../../../../static/files/abst.pdf')
