746cbff6f0a2420b12731ea822664038efe2a8b1
bushido <bushido@klab.ai.kyutech.ac.jp>
2017-01-27 14:10:27 +0900

    Split make command

--- modules/Tex2pdf.py
+++ modules/Tex2pdf.py
@@ -4,4 +4,5 @@ import shutil
 def tex2pdf(repository_dir, username):
     os.chdir(repository_dir)
     os.system('make')
+    os.system('make clean')
     shutil.move(repository_dir+'/abst.pdf', '../../../../static/files/' + username + '/abst.pdf')
